7bf9c487e9822c26b88dd0486d53164108df6f5d
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2026-01-04 13:00:35 +0800

    UPDATE!!!! : Installed React Router Dom and created files for different pages
    npm i react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,21 @@
-import { useState, useEffect } from 'react'
-import {getAllPosts, getOnePost, createPost, editPost, deletePost} from './api.js'
+// import { useState, useEffect } from 'react'
+// import {getAllPosts, getOnePost, createPost, editPost, deletePost} from './api.js'
 import './App.css'
 
 function App() {
-  const [posts, setPosts] = useState()
+  // Pages to make
 
-  function generatePost(){
-    let postData = {
-      title:"This is from frontend testing 3",
-      description:"This is testing 3 hehe",
-      content:"Walay pa awda content",
-      author:"bom bom  awdw pogi",
-      dateCreated: new Date()
-    }
-
-    createPost(postData)
-    .then((response) => {
-      console.log("Post created successfully:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error creating post:", error);
-    });
-  }
-
-  /*
-  useEffect(() => {
-    async function fetchPosts(){
-
-      let posts = await getAllPosts() //fetch all posts from backend in api.js
-
-      if (posts){
-        setPosts(posts)
-      }else{
-        throw new Error("Error fetching posts")
-      }
-      
-    }
- 
-    fetchPosts()
-  }, [])
-  */
+  // landing page
+  // HOme page
+  // Read Blog
+  // Create Blog
+  // Profile
+  // About
+  // Contact
 
   return (
     <>
-        <button onClick={generatePost}>Create Post</button>
+        
     </>
   )
 }
---
+++ frontend/src/pages/createBlog.jsx
@@ -0,0 +1,9 @@
+
+
+export function CreateBlog() {
+    return (
+        <>  
+            Create Blog Page
+        </>
+    )
+}
